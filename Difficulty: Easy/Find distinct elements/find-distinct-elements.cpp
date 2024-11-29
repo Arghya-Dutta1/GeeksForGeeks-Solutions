9be4db95bff221e95e4a3254836d0da6cebde6ff
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-11-29 23:50:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find distinct elements/find-distinct-elements.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+int distinct(vector<int>& arr) {
+    // Your code here
+    set<int> st;
+    for(auto i:arr) st.insert(i);
+    return st.size();
+}
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        cout << distinct(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
