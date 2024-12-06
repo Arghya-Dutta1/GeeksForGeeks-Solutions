2e6ac60dff2e01204640d4192dc7412680d8423a
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-06 12:19:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+    // Function to find the trapped water between the blocks.
+  public:
+    int trappingWater(vector<int> &height) {
+        // code here
+        int n=height.size();
+        vector<int> left(n);
+        vector<int> right(n);
+
+        left[0]=height[0];
+        for(int i=1;i<n;i++){
+    	    left[i]=max(left[i-1],height[i]);
+	    }
+	
+	    right[n-1]=height[n-1];
+	    for(int i=n-2;i>=0;i--){
+		    right[i]=max(right[i+1],height[i]);
+	    }
+
+        int s=0;
+	    for(int i=0;i<n;i++){
+		    s+=(min(left[i], right[i])-height[i]);
+	    }
+
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.trappingWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
