fcca9b83a06a8597db6e825941c675db26c15783
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-07 08:08:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// class Solution {
+//   public:
+//     // Function to count inversions in the array.
+//     int inversionCount(vector<int> &arr) {
+//         // Your Code Here
+//     }
+// };
+
+
+class Solution {
+  public:
+    int inversionCount(vector<int> &arr) {
+        return mergeSortAndCount(arr, 0, arr.size() - 1);
+    }
+
+  private:
+    int mergeAndCount(vector<int> &arr, int left, int mid, int right) {
+        vector<int> temp(right - left + 1);
+        int i = left, j = mid + 1, k = 0, inversions = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                inversions += (mid - i + 1); // All remaining elements in the left part are inversions
+            }
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (int x = left; x <= right; x++)
+            arr[x] = temp[x - left];
+
+        return inversions;
+    }
+
+    int mergeSortAndCount(vector<int> &arr, int left, int right) {
+        if (left >= right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int inversions = mergeSortAndCount(arr, left, mid);
+        inversions += mergeSortAndCount(arr, mid + 1, right);
+        inversions += mergeAndCount(arr, left, mid, right);
+
+        return inversions;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n;
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+        Solution obj;
+        cout << obj.inversionCount(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
