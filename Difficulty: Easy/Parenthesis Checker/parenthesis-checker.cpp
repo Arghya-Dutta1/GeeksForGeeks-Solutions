09bf137f552b2e0f0ba039b2279fde973c80edc9
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-01-16 13:38:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    char pop(stack<char>& s){
+        char c=s.top();
+        s.pop();
+        return c;
+    }
+  
+    bool isParenthesisBalanced(string& s) {
+        // code here
+        stack<char> st;
+        for(char i:s){
+            if(i == '(')
+                st.push(')');
+            else if(i == '{')
+                st.push('}');
+            else if(i == '[')
+                st.push(']');
+            else if(st.empty() || pop(st)!=i)
+                return false;
+        }
+        return st.empty();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        Solution obj;
+        if (obj.isParenthesisBalanced(a))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+// } Driver Code Ends
