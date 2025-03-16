690dcbf05f11084c16a0739a573037a06235a850
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-16 10:31:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        // code here
+        int ans=0, cmx=0, lmx=0;
+        for(int i=0;i<arr.size()-1;i++){
+            cmx=max(cmx, i+arr[i]);
+            if(lmx==i){
+                if(lmx==cmx) return -1;
+                ans++;
+                lmx=cmx;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, i, j;
+        vector<int> arr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.minJumps(arr) << endl << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
