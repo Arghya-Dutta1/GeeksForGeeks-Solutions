ded99971371e8c21d3a649b4b3451821ec91f55f
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-01 10:07:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -4,51 +4,60 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
-    vector<bool> vis;
-    vector<int> ans;
-    
-    void dfs(vector<vector<int>>& adj, int i){
-        if(vis[i]) return;
-        
-        vis[i]=true;
-        ans.push_back(i);
-        for(auto j:adj[i])
-            dfs(adj, j);
-    }
-    
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+    vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        vis.resize(adj.size(), false);
-        for(int i=0;i<adj.size();i++)
-            dfs(adj, i);
+        vector<bool> vis(adj.size()+1, false);
+        vector<int> ans;
+        stack<int> st;
+        st.push(0);
+        while(!st.empty()){
+            int i=st.top();
+            st.pop();
+            if(!vis[i]){
+                ans.push_back(i);
+                vis[i]=true;
+            }
+            for(auto it=adj[i].rbegin(); it!=adj[i].rend(); it++)
+                if(!vis[*it])
+                    st.push(*it);
+        }
         return ans;
     }
 };
+
+
 
 //{ Driver Code Starts.
 
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
+        int V;
+        cin >> V;
+        cin.ignore();
         vector<vector<int>> adj(
             V); // Use vector of vectors instead of array of vectors.
 
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
         }
 
         Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
+        vector<int> ans = obj.dfs(adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
