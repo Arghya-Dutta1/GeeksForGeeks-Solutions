e04e43fbb67af18bc7571a3263f04e5e8b4fd8fd
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-02 10:04:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -4,53 +4,63 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+    vector<int> bfs(vector<vector<int>> &adj) {
         // Code here
         vector<int> ans;
-        vector<int> vis(adj.size()+1, 0);
+        vector<bool> vis(adj.size()+1, false);
 	    queue<int> q;
 	    q.push(0);
-	    vis[0]=1;
+	    vis[0]=true;
 	    while(q.size()>0){
 		    int t=q.front();
 		    q.pop();
 		    ans.push_back(t);
 		    for(auto i:adj[t])
-			    if(vis[i]==0)
-			    	q.push(i), vis[i]=1;
+			    if(!vis[i])
+			    	q.push(i), vis[i]=true;
 	    }
 	    return ans;
     }
 };
+
+
 
 //{ Driver Code Starts.
 
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        // Now using vector of vectors instead of array of vectors
+        int V;
+        cin >> V;
+        cin.ignore();
+        // Use vector of vectors instead of array of vectors.
         vector<vector<int>> adj(V);
 
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
         }
 
         Solution obj;
-        vector<int> ans = obj.bfsOfGraph(adj);
+        vector<int> ans = obj.bfs(adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
         cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
