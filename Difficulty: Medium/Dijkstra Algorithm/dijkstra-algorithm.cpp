78eae29307831d028f0363f4eb4f9ca9cdd90b01
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-11 11:50:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User Function Template
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int, int>>> adj(V);
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            adj[u].push_back({v, w});
+        }
+
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push(make_pair(0, src));
+
+        while (!pq.empty()) {
+            pair<int, int> p = pq.top();
+            pq.pop();
+
+            int d = p.first;
+            int node = p.second;
+            for (int i = 0; i < adj[node].size(); ++i) {
+                int adjNode = adj[node][i].first;
+                int weight = adj[node][i].second;
+                if (dist[adjNode] > d + weight) {
+                    dist[adjNode] = d + weight;
+                    pq.push(make_pair(dist[adjNode], adjNode));
+                }
+            }
+        }
+
+        return dist;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> edges;
+        int i = 0;
+        while (i++ < E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            edges.push_back({u, v, w});
+            edges.push_back({v, u, w});
+        }
+        int src;
+        cin >> src;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(V, edges, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
