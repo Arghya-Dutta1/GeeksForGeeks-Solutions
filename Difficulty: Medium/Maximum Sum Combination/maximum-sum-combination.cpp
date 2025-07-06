f45f497ac2760b4206f6309015ad0fa033b1bd9e
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-07-06 12:42:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        // code here
+        int n=a.size();
+        priority_queue<pair<int,pair<int,int>>>pq;
+        set<pair<int,int>>vis;
+        sort(a.rbegin(),a.rend());
+        sort(b.rbegin(),b.rend());
+        pq.push({a[0]+b[0],{0,0}});
+        vis.insert({0,0});
+        vector<int> ans;
+        while(ans.size()<k){
+            auto top=pq.top();
+            pq.pop();
+            
+            int sum=top.first, i=top.second.first, j=top.second.second;
+            ans.push_back(sum);
+            
+            if(i<n-1&& vis.find({i+1,j})==vis.end()){
+                pq.push({a[i+1]+b[j],{i+1,j}});
+                vis.insert({i+1,j});
+            }
+            if(j<n-1&& vis.find({i,j+1})==vis.end()){
+                pq.push({a[i]+b[j+1],{i,j+1}});
+                vis.insert({i,j+1});
+            }
+        }
+        return ans;
+    }
+};
