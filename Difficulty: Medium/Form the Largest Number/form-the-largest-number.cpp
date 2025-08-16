9f942be81f7dafff2de2a4e2688f3329d50a7a71
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-08-16 12:10:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
@@ -1,52 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
     string findLargest(vector<int> &arr) {
         // code here
-        vector<string> v;
-        for(auto i:arr) v.push_back(to_string(i));
-        sort(v.begin(), v.end(), [](string& a, string& b){
-            return a+b>b+a;
+        vector<string> nums;
+        for (int num : arr) nums.push_back(to_string(num));
+        sort(nums.begin(), nums.end(), [](string &a, string &b) {
+            return a + b > b + a;
         });
-        
-        if(v[0]=="0") return "0";
-        
-        string ans="";
-        for(auto i:v) ans+=i;
-        return ans;
+        if (nums[0] == "0") return "0";
+        string result;
+        for (string &s : nums) result += s;
+        return result;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.findLargest(arr);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
